acebfebe437c8d38115035d650af422823e06129
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-04 16:52:32 +0100

    init

--- test2.php
+++ test2.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/.deploy-now/');
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
 echo "hello";
