25e03a5e3a95c2a3f4a666e8ed5079b4fb12324c
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-04 11:10:57 +0100

    init

---
+++ config/footer.php
@@ -0,0 +1,2 @@
+<?php
+echo '<footer><span>Built with PHP</span><span>v1.0 / 2026</span></footer>';
--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 <?php
 ini_set('display_errors', 1);
-echo getenv('APP_URL');
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-require_once __DIR__ . '/config/database.php';
-print_r($pdo);
+// echo getenv('APP_URL');
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
+// require_once __DIR__ . '/config/database.php';
+// print_r($pdo);
+
+require_once __DIR__ . '/config/footer.php';
