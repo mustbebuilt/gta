37bb66725d0466b0091a74acda4a7160449ef5ef
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-04 17:20:05 +0100

    init

--- test2.php
+++ test2.php
@@ -1,5 +1,12 @@
 <?php
 ini_set('display_errors', 1);
+if (file_exists(__DIR__ . '/.env')) {
+    echo ".env file exists at root.";
+} else {
+    echo "No .env file found at: " . __DIR__;
+}
+exit;
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
