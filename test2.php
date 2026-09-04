aec24672de434ef68ad0bc4537a90956082463c4
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-04 15:49:56 +0100

    init

--- test2.php
+++ test2.php
@@ -1,4 +1,9 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
+
 echo "hello";
 echo getenv('MAIL_HOST');
 echo 'DB_HOST: ' . (getenv('DB_HOST') ? 'SET' : 'NOT SET') . '<br>';
