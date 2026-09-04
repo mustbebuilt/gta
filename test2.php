ac42aa741fdaa1a908606a9516c12bdfe92a3224
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-04 16:03:20 +0100

    init

--- test2.php
+++ test2.php
@@ -2,10 +2,9 @@
 ini_set('display_errors', 1);
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__ . '/.deploy-now/gta/');
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->load();
 
 echo "hello";
-echo getenv('MAIL_HOST');
-echo 'DB_HOST: ' . (getenv('DB_HOST') ? 'SET' : 'NOT SET') . '<br>';
+echo getenv('TEST_ENV');
 echo "world";
