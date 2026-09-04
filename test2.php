5cd94569df073f30d10321935313d5e9850902c2
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-04 17:13:29 +0100

    init

--- test2.php
+++ test2.php
@@ -5,6 +5,8 @@ require_once __DIR__ . '/vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
 $dotenv->safeLoad();
 
+print_r($dotenv->load());
+
 echo "hello";
 echo getenv('DB_CONNECTION');
 echo "world";
