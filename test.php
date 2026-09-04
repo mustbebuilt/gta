5a38d30f00cd38b6881ce3a994575193e605cd19
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-04 10:42:53 +0100

    init

--- test.php
+++ test.php
@@ -1,6 +1,7 @@
 <?php
+echo getenv('APP_URL');
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-include '/config/database.php';
+require_once __DIR__ . '/config/database.php';
 print_r($pdo);
