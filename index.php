12f01f889bb9046d5edbcff1bb92506cca496ec7
martin@mustbebuilt.co.uk <martinmustbebuilt@gmail.com>
2026-09-05 07:08:25 +0100

    sat

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
+ini_set('display_errors', 1);
+// echo getenv('APP_URL');
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
-declare(strict_types=1);
+
+require_once __DIR__ . '/config/footer.php';
+require_once __DIR__ . '/config/database.php';
+
+echo getenv('MAIL_HOST');
+
+echo "Current directory is: " . __DIR__ . "<br>";
+echo "Files found in root:<br>";
+print_r(scandir(__DIR__));
+
+
+// print_r($pdo
 ?>
 <!DOCTYPE html>
 <html lang="en">
